1242124132727d24a8f22d6a72e7c80ace054c9c
BranceLee <475815059@qq.com>
2019-09-09 10:04:46 +0800

    add data struct content

---
+++ data_structures/queue.js
@@ -0,0 +1,28 @@
+const {Blockchain} = require('./linked_list')
+
+class Queue{
+    constructor(){
+        this.elements = new Blockchain();
+    }
+
+    isEmpty(){
+        return this.elements.size === 0
+    }
+
+    // add index at the end of the chain.
+    push(e){
+        this.elements.add(e);
+    }
+
+    // pop at the start of the chain
+    pop(){
+        if(this.isEmpty()){
+            throw new Error('Blockchain is null')
+        }
+        const data = this.elements.head.data
+        this.elements.removeAtIndex(0)
+        return data
+    }
+}
+
+module.exports = Queue
